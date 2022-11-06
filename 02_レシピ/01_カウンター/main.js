f4ec344c15699e63d56fe77d2ca111e014a7b31b
Squash-h <h.squash.5103@icloud.com>
2022-11-06 17:09:53 +0900

    テス。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,9 +1,19 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+const kakeruButton = document.getElementById("kakeru-button")
 
 let count = 0
 
 plusButton.onclick = function () {
   count += 1
+  display.textContent = count
+}
+minusButton.onclick = function () {
+  count -= 1
+  display.textContent = count
+}
+kakeruButton.onclick = function () {
+  count *= 2
   display.textContent = count
 }
